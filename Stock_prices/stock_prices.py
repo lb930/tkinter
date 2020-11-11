a86acfc0c5c429187d6df57dbf445c5d8e55e129
lb930 <59416844+lb930@users.noreply.github.com>
2020-11-11 21:08:46 +0100

    Added matplotlib chart

--- Stock_prices/stock_prices.py
+++ Stock_prices/stock_prices.py
@@ -14,59 +14,72 @@ class StockApp(Frame):
 
     def widgets(self):
         # width * height
-        root.geometry('500x400')
+        root.geometry('510x450')
         root.configure(bg='#FFFFFF')
 
         # Header
-        header_label = Label(text='Stock Prices', font=(
-            'Calibri', 22, 'bold'), bg='#FFFFFF')
-        header_label.place(x=30, y=15)
+        border_top = Label(text='_______', font=('Calibri light', 24, ''), bg='#FFFFFF', fg = '#383535')
+        border_top.place(x=30, y=0)
+        
+        border_bottom = Label(text='_______', font=('Calibri light', 24, ''), bg='#FFFFFF', fg = '#383535')
+        border_bottom.place(x=350, y=0)
+        
+        header_label = Label(text='STOCK PRICES', font=('Calibri light', 24, ''), bg='#FFFFFF', fg = '#383535')
+        header_label.place(x=157, y=15)
 
         # Enter your API key
-        api_key_label = Label(text='API key', font=(
-            'Calibri', 10), bg='#FFFFFF')
-        api_key_label.place(x=30, y=65)
+        api_key_label = Label(text='API key', font=('Calibri', 10), bg='#FFFFFF')
+        api_key_label.place(x=30, y=80)
 
-        self.api_key_field = Entry(
-            width=32, font=('Calibri', 10), bg='#F4F4F4')
+        self.api_key_field = Entry(width=32, font=('Calibri', 10), bg='#F4F4F4')
         self.api_key_field.config(show="*")
-        self.api_key_field.place(x=30, y=90)
+        self.api_key_field.place(x=30, y=105)
 
         # Enter an index
-        index_label = Label(text='Stock index', font=(
-            'Calibri', 10), bg='#FFFFFF')
-        index_label.place(x=280, y=65)
+        index_label = Label(text='Stock index', font=('Calibri', 10), bg='#FFFFFF')
+        index_label.place(x=275, y=80)
 
-        self.index_field = Entry(width=15, font=('Calibri', 10), bg='#F4F4F4')
-        self.index_field.place(x=280, y=90)
+        self.index_field = Entry(width=19, font=('Calibri', 10), bg='#F4F4F4')
+        self.index_field.place(x=275, y=105)
 
         # OK button
-        ok_btn = Button(text='OK', command=self.ok, font=(
-            'Calibri', 8), bg='#F4F4F4', width=5)
-        ok_btn.place(x=400, y=88)
+        ok_btn = Button(text='OK', command=self.ok, font=('Calibri', 8), bg='#F4F4F4', width=5)
+        ok_btn.place(x=430, y=103)
+        
+        # Symbol
+        self.symbol_var = StringVar()
+        self.symbol_label = Label(textvariable = self.symbol_var, font=('Calibri', 14, 'bold'), bg = '#FFFFFF', fg='#000000')
+        self.symbol_var.set('')
+        self.symbol_label.place(x = 50, y = 145)
+        
+        # Exchange
+        self.exchange_var = StringVar()
+        self.exchange_label = Label(textvariable = self.exchange_var, font=('Calibri', 9), bg = '#FFFFFF', fg='#6b6969')
+        self.exchange_var.set('')
+        self.exchange_label.place(x = 50, y = 169)
+        
+        # Last adjusted close
+        self.last_close_var = StringVar()
+        self.last_close_label = Label(textvariable = self.last_close_var, font=('Calibri', 18), bg = '#FFFFFF', fg='#000000')
+        self.last_close_var.set('')
+        self.last_close_label.place(x = 400, y = 155)
+
 
     def call_api(self):
-        ######## change API key and symbol once done ##############
-        # url  = requests.get(f'http://api.marketstack.com/v1/eod?access_key=996a4bf6ea79afe82bef5078dfcb1ac7&symbols=AAPL&limit=1000')
-        # current_stock = json.loads(url.text)
+        
+        if len(self.api_key_field.get()) == 0:
+            raise ValueError('Please provide a valid API key')
+        elif len(self.index_field.get()) == 0:
+            raise ValueError('Please provide a valid stock index')
+        else:
+            url  = requests.get(f'http://api.marketstack.com/v1/eod?access_key={self.api_key_field.get()}&symbols={self.index_field.get()}&limit=1000')
+            current_stock = json.loads(url.text)
 
-        current_stock = {'pagination': {'limit': 100, 'offset': 0, 'count': 100, 'total': 251
-    }, 'data': [
-        {'open': 118.32, 'high': 119.2, 'low': 116.13, 'close': 118.69, 'volume': 114457922.0, 'adj_high': 119.2, 'adj_low': 116.13, 'adj_close': 118.69, 'adj_open': 118.32, 'adj_volume': 114457922.0, 'symbol': 'AAPL', 'exchange': 'XNAS', 'date': '2020-11-06T00: 00: 00+0000'
-        },
-        {'open': 117.95, 'high': 119.62, 'low': 116.8686, 'close': 119.03, 'volume': 126387074.0, 'adj_high': 119.4137499474, 'adj_low': 116.66709394, 'adj_close': 118.8247672316, 'adj_open': 117.7466293789, 'adj_volume': 126387074.0, 'symbol': 'AAPL', 'exchange': 'XNAS', 'date': '2020-11-05T00: 00: 00+0000'
-        },
-        {'open': 114.14, 'high': 115.59, 'low': 112.35, 'close': 114.95, 'volume': 138235482.0, 'adj_high': 115.59, 'adj_low': 112.35, 'adj_close': 114.95, 'adj_open': 114.14, 'adj_volume': 138235482.0, 'symbol': 'AAPL', 'exchange': 'XNAS', 'date': '2020-11-04T00: 00: 00+0000'
-        },
-        {'open': 109.66, 'high': 111.49, 'low': 108.73, 'close': 110.44, 'volume': 107624448.0, 'adj_high': 111.49, 'adj_low': 108.73, 'adj_close': 110.44, 'adj_open': 109.66, 'adj_volume': 107624448.0, 'symbol': 'AAPL',
-    'exchange': 'XNAS', 'date': '2020-11-03T00: 00: 00+0000'
-        }]}
-
-        self.symbol = current_stock['data'][0]['symbol']
-        self.exchange = current_stock['data'][0]['exchange']
-        self.adj_close_latest = current_stock['data'][0]['adj_close']
-        self.df = pd.DataFrame(current_stock['data'])
-
+            self.symbol = current_stock['data'][0]['symbol']
+            self.exchange = current_stock['data'][0]['exchange']
+            self.adj_close_latest = current_stock['data'][0]['adj_close']
+            self.df = pd.DataFrame(current_stock['data'])
+        
     def format_df(self):
 
         self.df['date'] = self.df['date'].str[:10]
@@ -77,30 +90,49 @@ class StockApp(Frame):
 
     def draw_chart(self):
 
-        #plot data
-        fig, ax = plt.subplots(figsize=(2,1), dpi=50)
-        self.df.plot(ax=ax)
+        fig, ax = plt.subplots(figsize=(5,2.5), dpi=100)
+        self.df.plot(ax=ax, linewidth=0.2)
+        
+        ax.fill_between(x = self.df.index.values, y1 = self.df['adj_close'], color="skyblue", alpha=0.4)
 
         # #set major ticks format
         ax.xaxis.set_major_formatter(mdates.DateFormatter('%b %Y'))
 
+        # Remove axes and change colour
         ax.spines['top'].set_visible(False)
         ax.spines['right'].set_visible(False)
+        ax.spines['left'].set_visible(False)
+        ax.spines['bottom'].set_color('#9c9ea1')
+        ax.xaxis.label.set_color('#9c9ea1')
+        ax.tick_params(axis='both', colors='#9c9ea1', labelsize = 8)
 
+        
+        # Horizotal grid lines
+        ax.grid(axis='y', linestyle='--', linewidth = 0.4, color = '#edeef0')
+        
+        # Remove legend
         ax.get_legend().remove()
-
+        
+        # Add to tkinter window
         canvas = FigureCanvasTkAgg(fig, master=root)
         canvas.draw() # TK-Drawingarea
-        # Do I need to place both?
-        canvas.get_tk_widget().place(x = 30, y = 150)
-        canvas._tkcanvas.place(x = 30, y = 150)
+        canvas.get_tk_widget().place(x = 20, y = 185)
+        canvas._tkcanvas.place(x = 20, y = 185)
+        
+    def update_labels(self):
+        
+        self.symbol_var.set(self.symbol)
+        self.exchange_var.set(self.exchange)
+        self.last_close_var.set(self.adj_close_latest)
+        
+        root.update()
 
     def ok(self):
         self.call_api()
         self.format_df()
         self.draw_chart()
-
-
+        self.update_labels()
+        
 if __name__ == "__main__":
     root= Tk()
     app= StockApp(root)
